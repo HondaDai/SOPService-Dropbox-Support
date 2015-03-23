2e23f6282df2fadc20b1b5a163abb3830717624a
HondaDai <hondadai.tw@gmail.com>
2015-03-23 15:38:18 +0800

    rm dropbox global

--- dropbox-upload.php
+++ dropbox-upload.php
@@ -1,7 +1,13 @@
 <?php
 
-include_once "dropbox-global.php";
+ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(-1);
+
+require_once "dropbox-setting.php";
+require_once "dropbox-sdk-php-1.1.5/lib/Dropbox/autoload.php";
 use \Dropbox as dbx;
+
+$appInfo = dbx\AppInfo::loadFromJson($setting);
+$webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-SOPService/1.0");
 
 $authCode = $_POST['code'];
 
