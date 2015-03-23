1fbc7b0848d53eaee52f053da8be52e32eede08a
HondaDai <hondadai.tw@gmail.com>
2015-03-23 15:53:38 +0800

    form data

--- dropbox-upload-panel.php
+++ dropbox-upload-panel.php
@@ -1,6 +1,6 @@
 
 
-<form action="dropbox-upload.php" method="post">
+<form enctype="multipart/form-data" action="dropbox-upload.php" method="post">
   Code: <input type="text" name="code"><br>
   <input type="file" name="uploaded_file"><br>
   <input type="submit" value="Submit">
