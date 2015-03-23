2e809fdeed7d6280064b3fb4db8f7b8831a06485
HondaDai <hondadai.tw@gmail.com>
2015-03-23 14:57:29 +0800

    clear

--- dropbox-login.php
+++ dropbox-login.php
@@ -2,29 +2,20 @@
 
 ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(-1);
 
-include "setting.php";
+require_once "dropbox-setting.php";
 require_once "dropbox-sdk-php-1.1.5/lib/Dropbox/autoload.php";
 use \Dropbox as dbx;
 
-print_r( $setting);
+print_r($setting);
 
 $appInfo = dbx\AppInfo::loadFromJson($setting);
-
-//echo $setting;
-//print_r($setting);
-//echo json_encode($setting);
-
-//print_r($appInfo);
-//echo 'babok';
 
 $webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-SOPService/1.0");
 $authorizeUrl = $webAuth->start();
 
 echo $authorizeUrl;
 
-
 echo json_encode($setting);
-echo '...'.getenv('DROPBOX_KEY');
 
 
 ?>
